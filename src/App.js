6082b30d2defb8cd236e7b8733ce059690c4ab8f
PavelDemyanenko <mail@paveldemyanenko.com>
2017-06-16 05:37:06 -0400

    Init new react app
    - use create-react-app
    - without ejecting configs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import Header from './layout/Header';
-import Main from './layout/Main';
-import './stylesheets/App.css';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import './stylesheets/index.css';
+import './index.css';
 
-ReactDOM.render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
